375d4f40682a339eeb464a458577012746fe625c
efekanbicermac <efekan.bicer8@gmail.com>
2024-11-10 21:03:06 +0300

    remove fixGrids funcs because useless for new challenge

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 var allColumns = [][9]string{
 	{"5", "6", ".", "8", "4", "7", ".", ".", "."},
 	{"3", ".", "9", ".", ".", ".", "6", ".", "."},
@@ -52,26 +50,6 @@ func correctionCheck(num string, inputColumn, inputRow int) bool {
 	}
 	return true
 }
-func fixGrids() bool {
-	for rowNumber := 0; rowNumber < 9; rowNumber++ { //satirlari dondurur
-		for colIndex := 0; colIndex < 9; colIndex++ { // sutunlari dondurur
-			if allRows[rowNumber][colIndex] == "." { // eger "." var ise
-				for num := 1; num <= 9; num++ { // 1 ile 9 arasi rakamlar
-					numStr := fmt.Sprintf("%d", num)
-					if correctionCheck(numStr, rowNumber, colIndex) { // "." yerine neler gelebilir
-						placeNumber(numStr, rowNumber, colIndex) //gecici olarak tabloya yerlestir
-						if fixGrids() {                          //recursive call geri kalanini yerlestir
-							return true
-						}
-						removeNumber(rowNumber, colIndex) // eger kod buraya geldi yani false ise koyulan gecici numarayi degistirip sirasiyla diger rakama geciyor
-					}
-				}
-				return false //not ok
-			}
-		}
-	}
-	return true //hepsi ok
-}
 
 func placeNumber(numStr string, row, col int) {
 	allRows[row][col] = numStr
@@ -84,44 +62,35 @@ func removeNumber(row, col int) {
 }
 
 func main() {
-	if fixGrids() {
-		fmt.Println("islem tamamdir!")
-		// cozumu yazdir
-		for i := 0; i < 9; i++ {
-			fmt.Println(allRows[i])
-		}
-	} else {
-		fmt.Println("islem tamamlanmadi")
-	}
+
+	/*
+	   Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+	   A sudoku solution must satisfy all of the following rules:
+
+	   Each of the digits 1-9 must occur exactly once in each row.
+	   Each of the digits 1-9 must occur exactly once in each column.
+	   Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+	   The '.' character indicates empty cells.
+
+
+
+	   Example 1:
+
+
+	   Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+	   Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+	   Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+
+
+	   Constraints:
+
+	   board.length == 9
+	   board[i].length == 9
+	   board[i][j] is a digit or '.'.
+	   It is guaranteed that the input board has only one solution.
+
+	*/
 }
-
-/*
-Write a program to solve a Sudoku puzzle by filling the empty cells.
-
-A sudoku solution must satisfy all of the following rules:
-
-Each of the digits 1-9 must occur exactly once in each row.
-Each of the digits 1-9 must occur exactly once in each column.
-Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
-The '.' character indicates empty cells.
-
-
-
-Example 1:
-
-
-Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
-Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
-Explanation: The input board is shown above and the only valid solution is shown below:
-
-
-
-
-Constraints:
-
-board.length == 9
-board[i].length == 9
-board[i][j] is a digit or '.'.
-It is guaranteed that the input board has only one solution.
-
-*/
